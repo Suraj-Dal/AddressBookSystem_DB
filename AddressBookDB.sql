ea82c70cd401fb7b09a6e797b4df50f7e9dce08c
Suraj-Dal <dsurajdal10@gmail.com>
2022-06-18 01:18:08 +0530

    [Suraj]: Usecase 2 to create table address book is added.

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -1,2 +1,14 @@
 CREATE DATABASE Address_Book_Service_DB
 use Address_Book_Service_DB
+CREATE TABLE AddressBook
+(
+firstName varchar(20),
+lastName varChar(20),
+address varchar(100),
+city varchar(20),
+state varchar(20),
+zip int,
+phoneNumber varchar(12),
+email varchar(20)
+);
+select * from AddressBook
