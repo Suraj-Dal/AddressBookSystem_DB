098c2c9fb32bb7a158064627a2230063809bd161
Suraj-Dal <dsurajdal10@gmail.com>
2022-06-18 07:51:55 +0530

    [Suraj]: Usecase 8 to retrieve entries sorted by person's name for given city is added.

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -21,6 +21,10 @@ insert into AddressBook (PersonID,First_Name,Last_Name,Address,City,State,Zip,Ph
 values('SanketDal','Sanket','Dal','Vadgaon','Pune','Maharashtra','411047','9423371234','sanketdal@gmail.com');
 insert into AddressBook (PersonID,First_Name,Last_Name,Address,City,State,Zip,Phone_Number,Email) 
 values('MayureshD','Mayuresh','D','Marathahalli','Bangalore','Karnataka','560037','9906607712','mayuresh@gmail.com');
+insert into AddressBook (PersonID,First_Name,Last_Name,Address,City,State,Zip,Phone_Number,Email) 
+values('AdityaT','Aditya','T','Juhu','Mumbai','Maharashtra','400001','9867542310','adityat@gmail.com');
+insert into AddressBook (PersonID,First_Name,Last_Name,Address,City,State,Zip,Phone_Number,Email) 
+values('AkshayP','Akshay','P','Narhe','Pune','Maharashtra','4110047','9988776655','pakshay@gmail.com');
 select * from AddressBook;
 
 
@@ -42,3 +46,6 @@ select COUNT(State) from AddressBook;
 
 select State, count(*) as AddressCount
 from AddressBook group by (State);
+
+select * from AddressBook where City = 'Pune'
+order by (First_Name);
