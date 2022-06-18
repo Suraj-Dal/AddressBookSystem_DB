2e09cb0ecd5cabf246dbc1e21e5f6cb21137fa6e
Suraj-Dal <dsurajdal10@gmail.com>
2022-06-18 06:57:09 +0530

    [Suraj]: Usecase 4 to edit contact is added.

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -9,10 +9,13 @@ city varchar(20),
 state varchar(20),
 zip int,
 phoneNumber varchar(12),
-email varchar(20)
+email varchar(30)
 );
 select * from AddressBook
 
 insert into AddressBook (firstName,lastName,address,city,state,zip,phoneNumber,email) 
 values('Suraj','Dal','Rajgad','Pune','Maharashtra','412213','9876543210','surajdal@gmail.com');
+select * from AddressBook;
+
+update AddressBook set phoneNumber='9049091234' where firstName='Suraj';
 select * from AddressBook;
