79accf049ebec7c6c24fccb7cd10643454eaf526
Suraj-Dal <dsurajdal10@gmail.com>
2022-06-21 10:16:18 +0530

    [Suraj]: Usecase 11 to add person in family and friend is added.

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -1,5 +1,8 @@
+--Create Database
 CREATE DATABASE Address_Book_Service_DB
 use Address_Book_Service_DB
+
+--Create Table AddressBook
 CREATE TABLE AddressBook
 (
 PersonID varchar(10) not null,
@@ -15,6 +18,7 @@ PRIMARY KEY ("PersonID")
 );
 select * from AddressBook
 
+--Insert values into table
 insert into AddressBook (PersonID,First_Name,Last_Name,Address,City,State,Zip,Phone_Number,Email) 
 values('SurajDal','Suraj','Dal','Rajgad','Pune','Maharashtra','412213','9876543210','surajdal@gmail.com');
 insert into AddressBook (PersonID,First_Name,Last_Name,Address,City,State,Zip,Phone_Number,Email) 
@@ -27,16 +31,19 @@ insert into AddressBook (PersonID,First_Name,Last_Name,Address,City,State,Zip,Ph
 values('AkshayP','Akshay','P','Narhe','Pune','Maharashtra','4110047','9988776655','pakshay@gmail.com');
 select * from AddressBook;
 
-
+--update table
 update AddressBook set Phone_Number='9049091234' where PersonID='SurajDal';
 select * from AddressBook;
 
+--delete entries from table
 delete from AddressBook where PersonID='SanketDal';
 select * from AddressBook;
 
+--retrieve by city or state
 select * from AddressBook where City='Pune';
 select * from AddressBook where State='Karnataka';
 
+--counts by city or state
 select COUNT(city) from AddressBook;
 
 select City, count(*) as AddressCount
@@ -50,7 +57,7 @@ from AddressBook group by (State);
 select * from AddressBook where City = 'Pune'
 order by (First_Name);
 
-
+--alter table to add name and type of contact
 alter table AddressBook add Name varchar(300) not null,Type varchar(300) not null;
 select * from AddressBook;
 
@@ -64,3 +71,11 @@ select COUNT(State) from AddressBook;
 select Type, count(*) as NoOfContacts
 from AddressBook group by (Type);
 
+--add person to both family and friend
+insert into AddressBook (PersonID,First_Name,Last_Name,Address,City,State,Zip,Phone_Number,Email,Name,Type)
+values('Akash','Akash','D','Rajgad','Pune','Maharashtra','412213','9876554631','dakash@gmail.com','Brother','Family');
+
+insert into AddressBook (PersonID,First_Name,Last_Name,Address,City,State,Zip,Phone_Number,Email,Name,Type)
+values('Saket','Saket','V','Marathahalli','Bangalore','Karnataka','560037','9823432123','saket@gmail.com','Degree','Friend');
+
+alter table AddressBook add PersonID int identity not null primary key; 
